7504c920388f46277962d3a6a0f40ddbefa1df1d
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-01-02 20:53:25 +0900

    create CRUD page

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -10,6 +10,46 @@ class SpeciesController < ApplicationController
     @species = Species.find(params[:id])
   end
 
+  def new
+    @species = Species.new
+  end
+
+  def create
+    @species = Species.new(species_params)
+    if @species.save
+      redirect_to @species, notice: '作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @species = Species.find(params[:id])
+  end
+
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(species_params)
+      redirect_to @species, notice: '更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @species = Species.find(params[:id])
+    @species.destroy!
+    redirect_to species_path, notice: '削除しました'
+  end
+
+  def species_params
+    params.require(:species).permit(
+        :scientific_name, :japanese_name,
+        :chinese_name, :korean_name,
+        :english_name
+    )
+  end
+
   def search_params
     params.fetch(:q, {}).permit(:scientific_name_cont, :japanese_name_cont, :chinese_name_cont, :korean_name_cont, :english_name_cont)
   end
